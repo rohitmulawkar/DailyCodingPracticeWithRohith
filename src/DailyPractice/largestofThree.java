187efd53a285c8274ed0f02563ee343d05a3413a
Admin <rohitrm108@gmail.com>
2024-10-07 09:16:58 +0530

    7 october

---
+++ src/DailyPractice/largestofThree.java
@@ -0,0 +1,27 @@
+package DailyPractice;
+
+import java.util.Scanner;
+
+public class largestofThree {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter first number");
+        int num1 = sc.nextInt();
+        System.out.println("Enter second number");
+        int num2 = sc.nextInt();
+        System.out.println("Enter third number");
+        int num3 = sc.nextInt();
+        sc.close();
+        if (num1>num2 && num1>num3){
+            System.out.println(num1+" is largest");
+
+        }
+        else if (num2>num1 && num2>num3){
+            System.out.println(num2+" is largest");
+        }
+        else{
+            System.out.println(num3+" is largest");
+
+        }
+    }
+}
