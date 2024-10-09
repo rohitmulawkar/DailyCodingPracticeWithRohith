d080a997239a3e11e55318d33b017f990d81db6f
Admin <rohitrm108@gmail.com>
2024-10-09 08:51:44 +0530

    9 october

--- src/DailyPractice/PalindromeNumber.java
+++ src/DailyPractice/PalindromeNumber.java
@@ -10,8 +10,8 @@ public class PalindromeNumber {
         long num=original_num;
         long num3=0;
        while(num>0){
-           long num2= (int) (num%10);  // 1,4,1
-            num3=num3*10+num2; //1,14,141
+          // long num2= (int) (num%10);  // 1,4,1
+            num3=num3*10+(num%10); //1,14,141
             num =num/10; //14,1,0
 
         }
