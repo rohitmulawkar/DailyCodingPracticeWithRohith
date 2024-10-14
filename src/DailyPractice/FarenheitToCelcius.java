74dace4bff4acd5f552ca1c5ebb75032327c7893
Admin <rohitrm108@gmail.com>
2024-10-14 23:06:55 +0530

    14 October

--- src/DailyPractice/FarenheitToCelcius.java
+++ src/DailyPractice/FarenheitToCelcius.java
@@ -8,7 +8,7 @@ public class FarenheitToCelcius {
         System.out.println("Enter temperature in°F");
         float faranheit = sc.nextFloat();
         sc.close();
-        float celcius = (float) ((float)(faranheit-32.0f)*(5.0/9.0));
+        float celcius = (float) ((faranheit-32.0f)*(5.0/9.0));
         System.out.println("The temperature in Faranheigt is " +celcius+"°C");
 
 
